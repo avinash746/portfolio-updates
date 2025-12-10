738ef936db37790e240ca50dad5172043020bce9
AVINASH KUMAR <91619264+avinash746@users.noreply.github.com>
2025-12-11 01:31:11 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/avinash746",
   resume:
-    "https://drive.google.com/file/d/1Db6gNAmCOaeJaJ3DUIr-J86ZCH0UiB_u/view?usp=sharing",
+    "https://drive.google.com/file/d/1Byi5XJfHT1-61LzmmFg7Wop5lcUccfl-/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/avinash-kumar-085321222/",
   insta: "https://www.instagram.com/avinashkr7462/",
 };
