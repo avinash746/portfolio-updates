f69a97acb498c0dda56aebe3c7d60b79b5e761ac
AVINASH KUMAR <91619264+avinash746@users.noreply.github.com>
2025-12-31 23:15:44 +0530

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -102,7 +102,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2025 Avinash Kumar. All rights reserved.
+          &copy; 2026 Avinash Kumar. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
