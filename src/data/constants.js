369cd3b4115cc696a5208201fb238884ed70a27a
AVINASH KUMAR <91619264+avinash746@users.noreply.github.com>
2026-02-13 22:26:52 +0530

    Change resume link in constants.js
    Updated the resume link to a new Google Drive URL.

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/avinash746",
   resume:
-    "https://drive.google.com/file/d/1aZDonpbeHrAIq0mbD7sCFFFT75_NueJi/view?usp=sharing",
+    "https://drive.google.com/file/d/1HcGFi9lnkEzul7dl0zy0ZDqj4sJYW-BA/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/avinash-kumar-085321222/",
   insta: "https://www.instagram.com/avinashkr7462/",
 };
